4f4f64a387caa60268d3aac8ead6c68ecf712e0e
Anna Kushnir <62173987+anna-kushnir@users.noreply.github.com>
2022-04-08 14:37:06 +0300

    Create header.h

---
+++ lab_1/code/header.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+void OutputArr(long long[], int);
+long long* CreateArr(int, int);
+void BubbleSort(long long[], int);
+void BubbleSort(long long[], int, long long&, long long&);
+void SortAll(int);
+void SortAndOutput100(int);
+void SortAndOutput10_100_1000(long long*, long long*, long long*);
