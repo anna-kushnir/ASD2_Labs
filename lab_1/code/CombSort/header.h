2327007d921c5e0ff8ad259e2a57cd8112eb489a
Anna Kushnir <62173987+anna-kushnir@users.noreply.github.com>
2022-04-08 15:33:41 +0300

    Create header.h

---
+++ lab_1/code/CombSort/header.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+void CombSort(long long[], int);
+void CombSort(long long[], int, long long&, long long&);
+long long* CreateArr(int, int);
+void OutputArr(long long[], int);
+void SortAll(int);
+void SortAndOutput100(int);
+void SortAndOutput10_100_1000(int);
